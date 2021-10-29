9128809e24dd8470e42b194e348be8a457b1044d
Taif <74107163+Taif50@users.noreply.github.com>
2021-10-29 17:09:39 +0300

    Update create_DB_1707.sql

--- create_DB_1707.sql
+++ create_DB_1707.sql
@@ -218,10 +218,3 @@ from BOOK where B_STATUS ='available';
 --select--
 select * 
 from vBookStatus ;
---user add--
-alter session set "_oracle_script"= true; 
-create user bookrole identified by user;
-grant connect to bookrole;
-
-grant SELECT ON vBookStatus to bookrole;
-select * from SYS.vbookstatus;
