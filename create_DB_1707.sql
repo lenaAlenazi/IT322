7b71be0232b7bdc633a49a4e5ad870975798e9ae
rana-12344 <74002474+rana-12344@users.noreply.github.com>
2021-10-29 16:56:16 +0300

    Add files via upload

--- create_DB_1707.sql
+++ create_DB_1707.sql
@@ -14,7 +14,6 @@ LS_status varchar2(10),
 CONSTRAINT LS_expiry_resrvation PRIMARY KEY(LS_expiry_resrvation),
 CONSTRAINT LS_call_numberFK FOREIGN KEY(LS_call_number) REFERENCES LOAN (L_call_number));
 
-
 --branch--
 Create table Branch( 
 B_ID number(10),
@@ -23,6 +22,7 @@ B_phone number(10),
 BLoan_CallNumber number(10), 
 CONSTRAINT B_ID_pk PRIMARY KEY(B_ID), 
 CONSTRAINT BLoan_CallNumber_fk FOREIGN KEY(BLoan_CallNumber) REFERENCES LOAN(L_call_number));
+
 
 --branch_loc--
 Create table Branch_LOC( 
@@ -37,7 +37,6 @@ J_CallNumber number(10),
 J_Title varchar2(10),
 J_Availability varchar2(15),
 CONSTRAINT J_CallNumber_pk PRIMARY KEY(J_CallNumber)); 
-
 --JOURNAL_BRUNCH--
 Create table JOURNAL_BRUNCH( 
 JB_CallNumber number(10),
@@ -46,13 +45,14 @@ CONSTRAINT  JB_CallNumber_fk FOREIGN KEY(JB_CallNumber) REFERENCES JOURNAL(J_Cal
 CONSTRAINT  JB_ID_fk FOREIGN KEY(JB_ID) REFERENCES Branch(B_ID));
 
 
--- ·≈hesis --
+-- thesis table--
 Create table Thesis(
 T_number number(3),
 T_Author varchar2(20),
 T_location varchar2(20),
 T_Description varchar2(20),
 CONSTRAINT T_number_pk PRIMARY KEY(T_number));
+
 
 --thesis_branch--
 Create table thesis_branch (
@@ -69,6 +69,7 @@ R_duration_time  varchar2(20),
 R_location varchar2(20),
 R_type varchar2(20),
 CONSTRAINT R_room_No_pk PRIMARY KEY(R_room_No));
+
 
 --Room_branch--
 create table Room_Branch(
@@ -101,7 +102,6 @@ BRA_ID number(10),
 CONSTRAINT  RB_IDFK FOREIGN KEY(RB_ID) REFERENCES RECORD(R_ID),
 CONSTRAINT  BRA_IDFK FOREIGN KEY(BRA_ID) REFERENCES Branch(B_ID)) ;
 
-
 --Book--
 Create table Book(
 Book_ID  number(10),
@@ -111,12 +111,14 @@ B_status varchar2(20),
 B_name varchar2(20),
 CONSTRAINT Book_ID_pk PRIMARY KEY(Book_ID));
 
+
 --BOOK_BRUNCH--
 Create table BOOK_BRUNCH( 
 Bk_ID number(10),
 BB_ID number(10),
 CONSTRAINT  BB_ID_fk FOREIGN KEY(Bk_ID) REFERENCES Book(Book_ID),
 CONSTRAINT  BB_ID_fk FOREIGN KEY(BB_ID) REFERENCES Branch(B_ID));
+
 
 --Laon_book--
 Create table Laon_book( 
@@ -126,12 +128,14 @@ CONSTRAINT  B_IDFG FOREIGN KEY(B_ID) REFERENCES Book(Book_ID),
 CONSTRAINT  L_CALLNUMBERFG FOREIGN KEY(L_CALLNUMBER) REFERENCES LOAN(L_CALL_NUMBER));
 
 
+
 --collection--
 Create table COLLECTION ( 
 C_Name varchar2(20),
 C_Type varchar2(10),
 C_Serial_no  number(4),
 Constraint pk_C_Name Primary key (C_Name ));
+
 
 --COLLECT_BRANCH--
 Create table COLLECT_BRANCH( 
@@ -161,6 +165,7 @@ M_STATUS varchar2(10) ,
 CONSTRAINT M_EXPIRYPK PRIMARY KEY(M_EXPIRY),
 CONSTRAINT MS_IDFK FOREIGN KEY(MS_ID) REFERENCES member(M_ID));
 
+
 --mem_Address--
 Create table MEM_ADDRESS (
 M_ADDRESS  number(10),
@@ -174,7 +179,6 @@ MB_ID number(10),
 NB_ID number(10),
 CONSTRAINT MB_IDFK  FOREIGN KEY(MB_ID) REFERENCES member(M_ID),
 CONSTRAINT NB_IDFK FOREIGN KEY(NB_ID) REFERENCES Branch(B_ID)) ;
-
 
 --Fine--
 Create table Fine( 
@@ -196,8 +200,8 @@ MaxNoOfRenewal number(10),
 Prev_CallNumber number(10),
 Constraint P_Previlage_No_pk Primary key ( P_Previlage_No ),
 Constraint Prev_CallNumber_fk foreign key(Prev_CallNumber) references LOAN(L_call_number));
-
 --inser--
+
 insert into Book 
 values(001001,'Cornerstone Digital','Harper Lee','available','Kill a Mockingbird');
 
@@ -211,16 +215,13 @@ values(001111,'Roberts Brothers','Louisa May Alcott','not available','little wom
 create view vBookStatus 
 as select B_NAME, B_AUTHOR, B_STATUS
 from BOOK where B_STATUS ='available';
-
-
---select command--
+--select--
 select * 
 from vBookStatus ;
-
-
 --user add--
 alter session set "_oracle_script"= true; 
-create user userit identified by user;
-grant connect to userit;
-grant SELECT ON vBookStatus to userit;
+create user bookrole identified by user;
+grant connect to bookrole;
+
+grant SELECT ON vBookStatus to bookrole;
 select * from SYS.vbookstatus;
